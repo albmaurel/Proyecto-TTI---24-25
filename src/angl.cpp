72a166375bef3e7dae800266f519c0404c571955
albmaurel <maurelalberto@gmail.com>
2025-05-21 16:42:13 +0200

    FINAL

--- src/angl.cpp
+++ src/angl.cpp
@@ -31,7 +31,7 @@ double angl(Matrix vec1, Matrix vec2){
     double magv2 = norm(vec2);
     double theta  = 0.0;
 	double temp;
-    if (magv1*magv2 > small^2) {
+    if (magv1*magv2 > pow(small,2)) {
         temp= dot(vec1,vec2) / (magv1*magv2);
         if (abs( temp ) > 1.0) {
             double sign = temp > 0 ? 1 : -1;
